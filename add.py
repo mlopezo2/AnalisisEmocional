f3ae35bfa329b131328a0e3fe2074ac466831fba
mlopezo2 <160878713+mlopezo2@users.noreply.github.com>
2024-04-10 15:21:59 -0500

    Update add.py

--- add.py
+++ add.py
@@ -4,4 +4,4 @@ from PIL import Image
 st.title("Análisis Emocional")
 st.header("Plataforma de expresión y liberación emocional.")
 st.write("A través de esta plataforma podrás ")
-image = Image.open("FeelingWheel.jpg")
+image = Image.open("FeelingsWheel.jpg")
