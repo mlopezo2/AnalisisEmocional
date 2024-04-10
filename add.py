f4a659000f64a418e71e0e4e326ec18f02ebea5b
mlopezo2 <160878713+mlopezo2@users.noreply.github.com>
2024-04-10 15:17:32 -0500

    Update add.py

--- add.py
+++ add.py
@@ -1,1 +1,7 @@
 import streamlit as st
+from PIL import Image
+
+st.title("Análisis Emocional")
+st.header("Plataforma de expresión y liberación emocional.")
+st.write("A través de esta plataforma podrás ")
+image = Image.open("FeelingWheel.jpg")
