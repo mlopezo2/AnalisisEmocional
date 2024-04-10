11a980ad40292c980c1dc8b3eee80d987279e3fe
mlopezo2 <160878713+mlopezo2@users.noreply.github.com>
2024-04-10 18:41:15 -0500

    Update add.py

--- add.py
+++ add.py
@@ -20,11 +20,12 @@ st.subheader("Guía Inicial")
 st.write("¿Como te sientes hoy?")
 resp1 = st.checkbox("Alegría")
 resp2 = st.checkbox("Tristeza")
-  if resp1:
-    st.write("La alegría nos permite disfrutar diferentes aspectos de la vida y generar actitudes positivas frente a estos. También favorece el aprendizaje y la memoria")
-  if resp2:
-    st.write("La alegría nos permite disfrutar diferentes aspectos de la vida y generar actitudes positivas frente a estos. También favorece el aprendizaje y la memoria")
-    
+if resp1:
+  st.write("La alegría nos permite disfrutar diferentes aspectos de la vida y generar actitudes positivas frente a estos. También favorece el aprendizaje y la memoria")
+if resp2:
+  st.write("La alegría nos permite disfrutar diferentes aspectos de la vida y generar actitudes positivas frente a estos. También favorece el aprendizaje y la memoria")
+
+
 st.subheader("¿Cómo te sientes hoy? Escribe una palabra que describa tu estado emocional.")
 
 translator = Translator()
